5ac33507aca2a47ecf4055a4103bbf9ecaa6f32d
Olutomiwa Oyegbola <olutomiwa.oyegbola@gmail.com>
2026-06-04 16:04:25 -0500

    made db model optional

--- backend/model.py
+++ backend/model.py
@@ -11,10 +11,10 @@ class PostModel(Base):
     __tablename__ = "post_table"
     
     id: Mapped[UUID] = mapped_column(primary_key=True, index= True, default=uuid.uuid4)
-    title: Mapped[str] 
-    content: Mapped[str]
-    published: Mapped[bool] = mapped_column(server_default=text("False"))
-    rating: Mapped[Decimal] = mapped_column(Numeric(precision=4, scale=2))
+    title: Mapped[str | None]
+    content: Mapped[str | None] 
+    published: Mapped[bool | None] = mapped_column(server_default=text("False"))
+    rating: Mapped[Decimal | None] = mapped_column(Numeric(precision=4, scale=2))
     created_at: Mapped[datetime] = mapped_column(server_default=text("now()"))
     
     
@@ -22,9 +22,9 @@ class User(Base):
     __tablename__ = "user"
     
     id: Mapped[UUID] =  mapped_column(primary_key= True, index=True, default=uuid.uuid4, unique= True, nullable=False )
-    username: Mapped[str]
-    email: Mapped[str] = mapped_column( unique= True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
+    username: Mapped[str | None] = mapped_column(unique=True)
+    email: Mapped[str | None] = mapped_column( unique= True)
+    password: Mapped[str | None] = mapped_column(nullable=False)
     created_at: Mapped[datetime] = mapped_column(server_default=text("now()"))
     
     
