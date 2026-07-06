279db8784e601cb2bf1dd6ddb4678a6b7b9053d6
Olutomiwa Oyegbola <olutomiwa.oyegbola@gmail.com>
2026-07-06 15:51:16 -0500

    finetuned code

--- backend/main.py
+++ backend/main.py
@@ -18,9 +18,12 @@ Base.metadata.create_all(bind=engine)
 
 from fastapi.middleware.cors import CORSMiddleware
 
+origins = []
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
+    allow_credentials = True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
