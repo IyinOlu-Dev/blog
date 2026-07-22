bd0d6144b66db7d812375400084d5c38eb7b0a20
Olutomiwa Oyegbola <olutomiwa.oyegbola@gmail.com>
2026-07-22 13:30:34 -0500

    fixed configuration bug

--- backend/oauth.py
+++ backend/oauth.py
@@ -34,7 +34,7 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session= Depends(g
     credentials_exception = HTTPException(
         status_code= status.HTTP_401_UNAUTHORIZED,
         detail= "Could not validate credentials",
-        headers= {"WWW_Authenticate": "Bearer"}
+        headers= {"WWW-Authenticate": "Bearer"}
     )
     
     try:
