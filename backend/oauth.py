6892534bbeed74e10f4249a2649dabb011b8c586
IyinOlu-Dev <olutomiwa.oyegbola@gmail.com>
2026-09-10 21:10:53 -0500

    changed token minuted to 30

--- backend/oauth.py
+++ backend/oauth.py
@@ -15,7 +15,7 @@ load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
